3a9cc3de06fa1556b242da83974471406d4cc6a4
OutKast <educaleft@gmail.com>
2016-07-12 23:24:28 +0100

    vt6ca2_original
    Aplicación tal como quedó en el Videotutorial 5 del Curso de Angular 2

---
+++ timer.ts
@@ -0,0 +1,60 @@
+import { Component } from '@angular/core';
+import { bootstrap } from '@angular/platform-browser-dynamic';
+
+@Component({
+  selector: 'timer',
+  template: `
+    <div class="text-center">
+      <img src="assets/img/reloj.png" alt="Reloj">
+      <h1> {{ minutes }}:{{ seconds | number: '2.0' }} </h1>
+      <p>
+        <button (click)="togglePause()"
+          class="btn btn-danger">
+          {{ buttonLabel }}
+        </button>
+      </p>
+    </div>
+`
+})
+
+
+class TimerComponent {
+  minutes: number;
+  seconds: number;
+  isPaused: boolean;
+  buttonLabel: string;
+
+  constructor() {
+    this.resetTimer();
+    setInterval(() => this.tick(), 1000);
+  }
+
+  resetTimer(): void {
+    this.isPaused = true;
+    this.minutes = 24;
+    this.seconds = 59;
+    this.buttonLabel = 'Start';
+  }
+
+  private tick(): void {
+    if (!this.isPaused) {
+      this.buttonLabel = 'Pause';
+
+      if (--this.seconds < 0) {
+        this.seconds = 59;
+        if (--this.minutes < 0) {
+          this.resetTimer();
+        }
+      }
+    }
+  }
+
+  togglePause(): void {
+    this.isPaused = !this.isPaused;
+    if (this.minutes < 24 || this.seconds < 59) {
+      this.buttonLabel = this.isPaused ? 'Resume' : 'Pause';
+    }
+  }
+}
+
+bootstrap(TimerComponent);
